9d8a1f01e54710259c67e1e90b7c1b269d000bcf
MkySS <62342977+MkySS@users.noreply.github.com>
2020-04-21 18:25:17 +0100

    commit inicial

--- app/src/main/java/com/example/covid_19/Sintomas.java
+++ app/src/main/java/com/example/covid_19/Sintomas.java
@@ -2,6 +2,7 @@ package com.example.covid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Sintomas extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class Sintomas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sintomas);
+        Intent intent = getIntent();
     }
 }
